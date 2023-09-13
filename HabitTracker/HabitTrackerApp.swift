bdacc85b7bd32e7ef99cfe917b4d9bacadd96b8a
rishi-singh26 <rishisingh0831@gmail.com>
2023-09-13 22:00:15 +0530

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Rishi Singh on 13/09/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
